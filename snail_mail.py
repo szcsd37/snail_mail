d99fcfd2301fb102a23412683605b25f91ac6dee
szcsd37 <szucs.dominik@diak.szbi-pg.hu>
2024-11-25 08:42:33 +0100

    Update snail_mail.py

--- snail_mail.py
+++ snail_mail.py
@@ -1,27 +1,5 @@
 email = input("Your email address: ")
 
-# "hello.worldcom"    => An email address has to contain a '@' character!
-# "he@llo@world.com"  => An email address cannot contain more than one '@' characters!
-# "@world.com"        => The username before the '@' character cannot be empty!
-# "hello@"            => The domain after the '@' character cannot be empty!
-# "hello@worldcom"    => An email address has to contain at least one '.' character!
-# "hell.o@worldcom"   => The domain has to contain at least one '.' character!
-# "he.llo@worldcom."  => The top-level domain cannot be empty!
-# "he.llo@worldco.m"  => The top-level domain has to be at least two characters long!
-# ".hello@world.com"  => The username cannot start with a '.' character!
-# "he.llo@.world.com" => The domain cannot start with a '.' character!
-# "hello@world.com"   => Valid email address :)
-
-
-length_of_email = len(email)
-number_of_at_characters = email.count("@")
-number_of_dot_characters = email.count(".")
-position_of_at = email.find("@")
-
-position_of_first_dot = email.find(".")
-position_of_last_dot = email.rfind(".")
-position_of_first_dot_after_the_at = email.find(".", position_of_at)
-
 
 error_message_no_at = "An email address has to contain a '@' character!"
 error_message_too_many_at = "An email address cannot contain more than one '@' characters!"
@@ -34,5 +12,55 @@ error_message_short_tld = "The top-level domain has to be at least two character
 error_message_no_domain = "The domain after the '@' character cannot be empty!"
 error_message_invalid_username = "The username cannot start with a '.' character!"
 
-ok_message = "Valid email address :)"
+ok_message = "Valid email."
+
 is_valid = True
+
+number_of_at_characters = email.count("@")
+if number_of_at_characters == 0:
+    print(error_message_no_at)
+    is_valid = False
+elif number_of_at_characters > 1:
+    print(error_message_too_many_at)
+    is_valid = False
+
+elif email.find('@') == 0:
+    print(error_message_no_username)
+    is_valid = False
+elif email[0] == ".":
+    print(error_message_invalid_username)
+    is_valid = False
+
+else:
+    position_of_at = email.find("@")
+    username = email[:position_of_at]
+    domain = email[position_of_at + 1:]
+
+    if not domain:
+        print(error_message_no_domain)
+        is_valid = False
+
+    elif domain[0] == ".":
+        print(error_message_no_server_name)
+        is_valid = False
+
+    elif "." not in domain:
+        print(error_message_no_dot_in_domain)
+        is_valid = False
+
+    elif domain.endswith("."):
+        print(error_message_no_tld)
+        is_valid = False
+
+    else:
+        domain_parts = domain.split(".")
+        if len(domain_parts[-1]) < 2:
+            print(error_message_short_tld)
+            is_valid = False
+
+if "." not in email:
+    print(error_message_no_dot)
+    is_valid = False
+
+if is_valid:
+    print(ok_message)
